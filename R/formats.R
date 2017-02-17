352317763bcc405893d7e76d72dfb9830b7eb301
Dirk Eddelbuettel <edd@debian.org>
2017-02-17 15:16:53 -0600

    new ymd() format too terrible to be in anytime

---
+++ R/formats.R
@@ -0,0 +1,19 @@
+#' Format a Date(time) object as ymd
+#'
+#' @param pt A \code{POSIXt} Datetime or a \code{Date} object
+#'
+#' @return A character object formatted as \sQuote{YYYYMMDD}
+#' @author Dirk Eddelbuettel
+#' @examples
+#' ymd(anytime("2016-09-01 10:11:12.123456"))
+#' ymd(anydate("2016-Sep-01"))
+ymd <- function(pt) {
+    if (inherits(pt, "POSIXt"))
+        return(format.POSIXct(pt, "%Y%m%d"))
+    else if (inherits(pt, "Date"))
+        return(format.Date(pt, "%Y%m%d"))
+
+    warning("Inapplicable object: ", pt)
+    invisible(NULL)
+}
+
