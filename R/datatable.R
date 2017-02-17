050cc396d2b3b2f5eb27ab120553872853599ef6
Dirk Eddelbuettel <edd@debian.org>
2017-02-17 15:17:19 -0600

    slightly better converter

--- R/datatable.R
+++ R/datatable.R
@@ -2,7 +2,9 @@ as.data.table.xts <- function(x) {
     dt <-  data.table(date=as.IDate(index(x)),
                       time=as.ITime(index(x)),
                       micros=round(1e6*(as.numeric(index(x)-trunc(index(x))))),
+                      pt=as.numeric(index(x)),
                       coredata(x))
-    setkey(dt, date, time, micros)
+    setNumericRounding(0)
+    setkey(dt, pt, date, time, micros)
     dt
 }
