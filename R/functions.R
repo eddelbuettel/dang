cdae9e08f223dca927bd1e0f509e4d7c3e374eeb
Dirk Eddelbuettel <edd@debian.org>
2017-08-14 08:10:36 -0500

    new export, new argument

--- R/functions.R
+++ R/functions.R
@@ -5,14 +5,16 @@
 ##' debugging via \code{browser()} and other helper functions.
 ##' @title Assign formal arguments from function
 ##' @param f A function
+##' @param env An environment to assign the values to, defaults
+##' to global environment,
 ##' @return Nothing, but a side effect of assignment in global
-##'  environment
+##' environment
 ##' @author Dirk Eddelbuettel
-assignFormals <- function(f) {
+assignFormals <- function(f, env=.GlobalEnv) {
     ff <- formals(f)
     for (n in names(ff)) {
         txt <- sprintf("%s <- %s", n, eval(ff[[n]]))
-        assign(n, eval(ff[[n]]), envir=.GlobalEnv)
+        assign(n, eval(ff[[n]]), envir=env)
     }
     invisible(NULL)
 }
