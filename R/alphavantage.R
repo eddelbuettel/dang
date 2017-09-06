f36757bd28e52fcfccb5ed83d14aeb564c77ddf9
Dirk Eddelbuettel <edd@debian.org>
2017-09-06 12:29:56 -0500

    new function alphavantage

---
+++ R/alphavantage.R
@@ -0,0 +1,17 @@
+##' Fetch a real-time market data series from AlphaVantage
+##'
+##' Several optional parameters could be set, but are not currently.
+##' @title Retrieve real-time data from AlphaVantage
+##' @param sym Character value for the ticker
+##' @return A data.table object
+##' @author Dirk Eddelbuettel
+alphavantage <- function(sym) {
+    cmd <- paste0("https://www.alphavantage.co/query?",
+                  "function=TIME_SERIES_INTRADAY&",
+                  "symbol=", sym,
+                  "&interval=1min&",
+                  "apikey=", getOption("alphavantageKey", "demo"), "&",
+                  "datatype=csv&",
+                  "outputsize=compact")
+    data <- fread(cmd, showProgress=FALSE)
+}
