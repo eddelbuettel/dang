b845733c2287f35587e1f13c0b154acc7bbd005f
Dirk Eddelbuettel <edd@debian.org>
2019-03-10 12:40:16 -0500

    updated inGit and added getGitRoot

--- R/inGit.R
+++ R/inGit.R
@@ -1,21 +1,33 @@
-##' This function recursively ascends the filesystem tree from the
-##' given directory until it either finds a directory \code{.git}, or
-##' the top-level directory to abort the search.
+
+##' The \code{getGitRoot()} function recursively ascends the
+##' filesystem tree from the given directory until it either finds a
+##' directory \code{.git}, or the top-level directory to abort the
+##' search. The root directory of the \code{git} repository is
+##' returned, with an exmpty string in the case of no repository.  The
+##' \code{inGit()} function turns this into boolean predicate returning
+##' either \code{TRUE} or \code{FALSE}.
 ##'
-##' On Windows, \code{FALSE} is returned unconditionally.
 ##' @title Are we in a \code{git} repository?
 ##' @param cwd The start directory, default to the current working
 ##' directory
-##' @return The path of the directory containing the \code{.git}
-##'  directory, ie the project root directory, or an empty string
-##'  in case the search started outside a \code{git} directory.
+##'
+##' @return For \code{getGitRoot()}, the path of the directory containing
+##'  the \code{.git} directory, ie the project root directory, or an
+##'  empty string in case the search started outside a \code{git} directory.
+##'  The \code{inGit()} function returns a boolean as to whether a \code{git}
+##'  repository was found or not.
 ##' @author Dirk Eddelbuettel
 ##' @examples
 ##' inGit()
-inGit <- function(cwd=getwd()) {
+getGitRoot <- function(cwd=getwd()) {
     isTop <- function(cwd) normalizePath(cwd) == normalizePath(file.path(cwd, ".."))
     if (dir.exists(file.path(cwd, ".git"))) return(cwd)
     parent <- normalizePath(file.path(cwd, ".."))
     if (parent == "/" || isTop(parent)) return("")
     return(inGit(parent))
+}
+
+##' @rdname getGitRoot
+inGit <- function(cwd=getwd()) {
+    getGitRoot(cwd) != ""
 }
