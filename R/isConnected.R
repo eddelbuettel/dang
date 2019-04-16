27a8bfe22166c44e2d1ca1a8e4b3bde3e8a635ca
Dirk Eddelbuettel <edd@debian.org>
2019-04-16 13:24:33 -0500

    new function isConnected, new minor release

---
+++ R/isConnected.R
@@ -0,0 +1,17 @@
+##' Function to (heuristically) test for a network connection by attempting
+##' to connect to a given website.
+##'
+##' The main page of Google is used as a proxy for overall network
+##' connectivity as Google is generally 'network-close' and the page
+##' is relatively small.  If a network is unavailable this fails
+##' generally already on domain name service resolution. Special thanks
+##' to Barry and Brodie for a very helpful discussion re-discovering this
+##' function.
+##' @title Is the current session (networked) and connected?
+##' @param site Character variable with site to try to connect to,
+##' defauls to \code{http://www.google.com}
+##' @return A boolean value indicating networking status
+##' @author Dirk Eddelbuettel
+isConnected <- function(site="https://www.google.com") {
+    suppressWarnings(inherits(try(open(url(site)), silent=TRUE), "try-error"))
+}
