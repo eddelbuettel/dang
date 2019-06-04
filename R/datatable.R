ec18cde8ba5aea1c482bdddbf07d67d5d4e5f58a
Dirk Eddelbuettel <edd@debian.org>
2019-06-04 07:04:13 -0500

    improved as.data.table.xts

--- R/datatable.R
+++ R/datatable.R
@@ -5,25 +5,50 @@
 ##' \code{setNumericRounding(0)} is executed too,
 ##' @title Convert an xts object into a data.table
 ##' @param x An xts object
+##' @param ... Catch-all arguments passed on to methods
 ##' @return A data.table object with new columns date, time, micros,
 ##'  pt providing, respectively the data as \code{IDate}, time as
 ##' \code{ITime}, microseconds (rounded) and numeric
 ##' \code{POSIXct}.
 ##' @author Dirk Eddelbuettel
 as.data.table.xts <- function(x) {
-    if (requireNamespace("zoo", quietly=TRUE) && requireNamespace("data.table", quietly=TRUE)) {
+    if (!requireNamespace("zoo", quietly=TRUE) || !requireNamespace("data.table", quietly=TRUE)) {
+        stop("Cannot covert to 'data.table' without the 'zoo' package.", call.=FALSE)
+    }
+    if (class(zoo::index(x)) == "Date") {
+        date <- NULL
+        dt <-  data.table::data.table(date = data.table::as.IDate(zoo::index(x)),
+                                      zoo::coredata(x))
+        data.table::setkey(dt, date)
+        dt
+    } else {
         pt <- time <- micros <- NULL        # to make R CMD check happy
         dt <-  data.table::data.table(date = data.table::as.IDate(zoo::index(x)),
                                       time = data.table::as.ITime(zoo::index(x)),
                                       micros = round(1e6*(as.numeric(zoo::index(x) - trunc(zoo::index(x))))),
                                       pt = as.numeric(zoo::index(x)),
-                          zoo::coredata(x))
+                                      zoo::coredata(x))
         data.table::setNumericRounding(0)
         data.table::setkey(dt, pt, date, time, micros)
         dt
-    } else {
-        stop("Cannot covert to 'data.table' without the 'zoo' package/", call.=FALSE)
     }
+}
+
+
+##' @rdname as.data.table.xts
+as.data.table <- function(x) {
+    if (!requireNamespace("data.table", quietly=TRUE)) {
+        stop("Cannot covert to 'data.table' without the 'data.table' package.", call.=FALSE)
+    }
+    UseMethod("as.data.table")
+}
+
+##' @rdname as.data.table.xts
+as.data.table.default <- function(x, ...) {
+    if (!requireNamespace("data.table", quietly=TRUE)) {
+        stop("Cannot covert to 'data.table' without the 'data.table' package.", call.=FALSE)
+    }
+    as.data.table.default(x, ...)
 }
 
 ## cf http://stackoverflow.com/a/27358897/143305
