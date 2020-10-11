617fe41a6ab2f21427905671fc67cd309d9620be
Dirk Eddelbuettel <edd@debian.org>
2020-10-11 09:23:43 -0500

    roll minor, add explicit copyright header

--- src/checkNonAscii.cpp
+++ src/checkNonAscii.cpp
@@ -1,3 +1,30 @@
+
+// This file is part of file src/library/tools/src/text.c in the R sources e.g.
+//   https://github.com/wch/r-source/blob/trunk/src/library/tools/src/text.c
+// and has been modified / simplified to use the tidyCpp API wrapper package
+//
+// Those modifications are Copyright (c) 2020 - present Dirk Eddelbuettel
+// and released as usual under GPL (>= 2) as well.  The original is by R Core.
+
+
+//  R : A Computer Language for Statistical Data Analysis
+//  Copyright (C) 2003-2016   The R Core Team.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, a copy is available at
+//  https://www.R-project.org/Licenses/
+//
+
 #include <tidyCpp>
 
 extern "C" {
