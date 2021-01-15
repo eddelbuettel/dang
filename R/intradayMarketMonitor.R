4a3827f88d7e8cbeed9ddecd65e8534047e1ac15
Dirk Eddelbuettel <edd@debian.org>
2021-01-15 14:46:11 -0600

    first version of intradayMarketMonitor

---
+++ R/intradayMarketMonitor.R
@@ -0,0 +1,135 @@
+
+## cf https://gist.github.com/joshuaulrich/ee11ef67b1461df399b84efd3c8f9f67#file-intraday-sp500-r
+
+##' Intra-day Market Monitor for Security Prices
+##'
+##' This function periodically queries a public data source for a current price of given
+##' symbol and updates an intra-daily chart for that security. A working example is symbol
+##' \dQuote{^GSPC} for the S&P500 index which can be obtained in real-time during (New York)
+##' trading hours. Other symbols may work.
+##'
+##' An alternate version offering 24-hour coverage, for example for futures on Globex, is
+##' being prepared.
+##'
+##' The function could be further generalized in numerous way and should be considered
+##' \sQuote{alpha}. Current default values are a 15 second sleep, and fixed cut-off times
+##' for market open/close states. These will become configuration parameters in due course.
+##' @title Intra-day market monitor
+##' @param symbol A character variable with symbol understood by \code{getQuote} from
+##' package \pkg{quantmod}, default value is \dQuote{^GSPC}.
+##' @param defaultTZ A character variable with the (local) timezone used for displaying
+##' the data, default value is \dQuote{America/Chicagp}.
+##' @return Nothing is returned
+##' @seealso \url{https://gist.github.com/joshuaulrich/ee11ef67b1461df399b84efd3c8f9f67#file-intraday-sp500-r}
+##' @author Dirk Eddelbuettel extending and refactoring the original code by Josh Ulrich
+##' @examples
+##' if (requireNamespace("quantmod", quietly)) {           # only suggested packages used
+##'    suppressMessages({library(xts);library(quantmod)})  # dampen noise, add dang as needed
+##'    intradayMarketMonitor()
+##' }
+intradayMarketMonitor <- function(symbol = "^GSPC", defaultTZ = "America/Chicago") {
+    stopifnot(`The quantmod packages is required.`=requireNamespace("quantmod", quietly=TRUE))
+
+    x <- NULL
+    spfile <- .default_file(symbol)
+    if (file.exists(spfile)) {
+        x <- .most_recent_n_days(readRDS(spfile))
+        .show_plot(symbol, x)
+    }
+
+    market_closed <- TRUE
+    errored <- FALSE
+    prevVol <- 0
+    repeat {
+        curr_t <- Sys.time()
+        now_t <- xts::xts(, curr_t)
+        now <- xts::.indexhour(now_t)*100 + xts::.indexmin(now_t)
+        if (now >= 1500) {
+            .msg(zoo::index(now_t), "after close; setting NA, writing data and sleeping")
+            market_closed <- TRUE
+            ## we need three NA observations to plot a gap, so repeat three times
+            y <- xts::xts(data.frame(Open=rep(NA,3),High=NA,Low=NA,Close=NA,Volume=0),
+                          trunc(curr_t)+0:2)
+            x <- rbind(x, y)
+            saveRDS(x, spfile)
+            tgt <- as.POSIXct(paste(format(as.Date(curr_t)), "23:59:59"))
+            dt <- ceiling(as.numeric(difftime(tgt, curr_t, units="mins")))
+            Sys.sleep(dt*60)
+            next
+        } else if (now < 830) {
+            market_closed <- TRUE
+            tgt <- as.POSIXct(paste(format(as.Date(curr_t)), "08:29:00"))
+            dt <- max(1L, round(as.numeric(difftime(tgt, curr_t, units="mins"))))
+            .msg(zoo::index(now_t), "before open; sleeping for", dt, "min")
+            x <- .most_recent_n_days(x)      # subset data
+            Sys.sleep(dt*60)
+            next
+        } else if (now >= 830 && market_closed) {
+            .msg(zoo::index(now_t), "market open")
+            market_closed <- FALSE
+        }
+        y <- try(.get_data(symbol, defaultTZ), silent = TRUE)
+        if (inherits(y, "try-error")) {
+            .msg(curr_t, "Error:", attr(y, "condition")[["message"]])
+            errored <- TRUE
+            Sys.sleep(15)
+            next
+        } else if (errored) {
+            errored <- FALSE
+            .msg(curr_t, "...recovered")
+        }
+        v <- unname(zoo::coredata(quantmod::Vo(y))[1,1])
+        if (v > prevVol) {
+            x <- rbind(x, y)
+        }
+        prevVol <- v
+        if (nrow(x) >= 5) {
+            .show_plot(symbol, x, y)
+        }
+        Sys.sleep(15)
+    }
+    # may not get here if Ctrl-C aborted
+    saveRDS(x, spfile)
+}
+
+## unexported helper functions below
+
+.default_file <- function(symbol) {
+    nm <- paste0("intraday_", make.names(symbol), ".rds")
+    dd <- tools::R_user_dir("dang")
+    if (!dir.exists(dd)) dir.create(dd)
+    fname <- file.path(dd, nm)
+    fname
+}
+
+.most_recent_monday <- function() {
+    d <- Sys.Date()
+    while (as.POSIXlt(d)$wday != 1) d <- d - 1
+    d
+}
+
+.most_recent_n_days <- function(x, n=2, minobs=1500) {
+    tt <- table(as.Date(zoo::index(x)))
+    cutoff <- as.Date(names(head(tail(tt[tt>minobs], n), 1)))
+    x[ as.Date(zoo::index(x)) >= cutoff ]
+}
+
+.show_plot <- function(symbol, x, y) {
+    cname <- paste(symbol, format(quantmod::Cl(xts::last(na.omit(x)))), sep="\t")
+    if (!missing(y)) cname <- paste(cname, round(attr(y, "pct_change"), 5), sep = "\t")
+    cs <- quantmod::chart_Series(quantmod::Cl(x), name = cname)
+    plot(cs)
+}
+
+.get_data <- function(symbol, tz) {
+    quote <- quantmod::getQuote(symbol)
+    attr(quote$`Trade Time`, "tzone") <- tz
+    quote$Close <- quote$Last
+    xts::xts(quantmod::OHLCV(quote), quote[,"Trade Time"], pct_change = quote[,"% Change"])
+}
+
+.msg <- function(ts, ...) {
+    op <- options(digits.secs=3)
+    cat(format(ts), ..., "\n")
+    options(op)
+}
