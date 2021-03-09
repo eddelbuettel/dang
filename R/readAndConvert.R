dffefb06deca2b1efb33555d51836f0a2189f959
Dirk Eddelbuettel <edd@debian.org>
2021-03-08 19:28:23 -0600

    add note about iconv fidelity across OSs, or lack thereof

--- R/readAndConvert.R
+++ R/readAndConvert.R
@@ -4,6 +4,9 @@
 ##' The function is an adapted version of the one in the Rcpp Gallery post at
 ##' \url{https://gallery.rcpp.org/articles/iconv-via-r-header/} which is itself based on
 ##' \url{https://fishandwhistle.net/post/2021/using-rs-cross-platform-iconv-wrapper-from-cpp11/}.
+##' It is however worth pointing out that \sQuote{iconv} results have been seen to vary
+##' across operating systems. While it is \emph{portable} it does not guarantee identical
+##' outcomes across implementations: results on Windows have different from those on Unix OSs.
 ##' @title readAndConvert
 ##' @param filename Character variable with path a file with text in encoding
 ##' @param encoding Optional character variable with the encoding, if unset via the default
