30d606bc278f8e84d3d0e5079bc383dd53e0c0ab
Dirk Eddelbuettel <edd@debian.org>
2025-12-16 08:00:25 -0600

    Reorder microbenchmark results based on optional ordervar

--- R/reorderMicrobenchmarkResults.R
+++ R/reorderMicrobenchmarkResults.R
@@ -9,15 +9,18 @@
 #'
 #' @title Reorder microbenchmark Results
 #' @param res An object returned from `microbenchmark::microbenchmark`
-#' @param order An option character variable selecting a column to order on,
+#' @param ordervar An option character variable selecting a column to order on,
 #' defaults to 'median'
 #' @return A modified version of `res` with an ordering by `order`
-reorderMicrobenchmarkResults <- function(res, order="median") {
-    stopifnot("Argument 'res' must be a 'microbenchmark' result" = inherits(res, "microbenchmark"))
+reorderMicrobenchmarkResults <- function(res, ordervar="median") {
+    stopifnot("Argument 'res' must be a 'microbenchmark' result" =
+                  inherits(res, "microbenchmark"),
+              "Argument 'ordervar' must be a column name in 'summary(res)'" =
+                  is.finite(match(ordervar, names(summary(res)))))
 
     smry <- summary(res)
     res$expr <- factor(res$expr,
-                       levels = levels(res$expr)[order(smry[["median"]])],
+                       levels = levels(res$expr)[order(smry[[ordervar]])],
                        ordered = TRUE)
     res
 }
